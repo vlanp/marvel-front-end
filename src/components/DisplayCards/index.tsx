ad3e5a53d587cdb489fc30d18cd53df08bd07282
Valentin <guillaumevalentin76@gmail.com>
2024-06-24 00:46:08 +0200

    test

--- src/components/DisplayCards/index.tsx
+++ src/components/DisplayCards/index.tsx
@@ -56,7 +56,7 @@ const DisplayCards = <
     const fetchData = async () => {
       try {
         const url1 =
-          import.meta.env.VITE_TOTO +
+          import.meta.env.VITE_BACK_END_URL +
           finalEndpoint.endpoint1.endpoint +
           (params ? params : "") +
           "?skip=" +
@@ -66,12 +66,14 @@ const DisplayCards = <
             : "");
 
         console.log(url1);
-        console.log(import.meta.env.VITE_TOTO);
+        console.log(import.meta.env.VITE_BACK_END_URL);
 
         const url2 =
           finalEndpoint.endpoint2 &&
           params &&
-          import.meta.env.VITE_TOTO + finalEndpoint.endpoint2.endpoint + params;
+          import.meta.env.VITE_BACK_END_URL +
+            finalEndpoint.endpoint2.endpoint +
+            params;
 
         const arrayOfPromises: [
           Promise<AxiosResponse<T>>,
